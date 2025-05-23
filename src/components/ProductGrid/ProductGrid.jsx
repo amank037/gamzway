ac96b309228e394dddd09f6748e8857ee6f5d629
Aman <amank.business@outlook.com>
2025-05-23 12:38:18 +0530

    added floating animations in product grid

--- src/components/ProductGrid/ProductGrid.jsx
+++ src/components/ProductGrid/ProductGrid.jsx
@@ -4,64 +4,75 @@ function ProductGrid() {
   return (
     <div className='product-grid'>
       <div className='product-grid-item'>
-
             <a href="">
               <div className="poker-image-stack">
-                <img src="/ProductImages/poker/poker-bg.png" alt="Poker BG" className="poker-stack-img poker-stack-bg" />
+              <img src="/ProductImages/poker/poker-bg.png" alt="Poker bg" className="poker-stack-img poker-stack-bg" />
                 <img src="/ProductImages/poker/poker-glow.png" alt="Poker Glow" className="poker-stack-img poker-stack-glow" />
-                <img src="/ProductImages/poker/poker-cards.png" alt="Poker Cards" className="poker-stack-img poker-stack-cards" />
-                <img src="/ProductImages/poker/poker-chips-left.png" alt="Poker Chips Left" className="poker-stack-img poker-stack-chips-left" />
-                <img src="/ProductImages/poker/poker-chips-right.png" alt="Poker Chips Right" className="poker-stack-img poker-stack-chips-right" />
+                <img src="/ProductImages/poker/poker-card1.png" alt="Poker Cards" className="poker-stack-img poker-stack-card1" />
+                <img src="/ProductImages/poker/poker-card2.png" alt="Poker Cards" className="poker-stack-img poker-stack-card2" />
+                <img src="/ProductImages/poker/poker-card3.png" alt="Poker Cards" className="poker-stack-img poker-stack-card3" />
+                <img src="/ProductImages/poker/poker-card4.png" alt="Poker Cards" className="poker-stack-img poker-stack-card4" />
+                <img src="/ProductImages/poker/poker-chip-left-top.png" alt="Poker Chips Left" className="poker-stack-img poker-chip-left-top" />
+                <img src="/ProductImages/poker/poker-chip-left-bottom.png" alt="Poker Chips Left" className="poker-stack-img poker-chip-left-bottom" />
+                <img src="/ProductImages/poker/poker-chip-right-top.png" alt="Poker Chips Left" className="poker-stack-img poker-chip-right-top" />
+                <img src="/ProductImages/poker/poker-chip-right-bottom.png" alt="Poker Chips Left" className="poker-stack-img poker-chip-right-bottom" />
                 <img src="/ProductImages/poker/poker-text.png" alt="Poker Text" className="poker-stack-img poker-stack-text" />
               </div>
             </a>
-
+      </div>
+      <div className='product-grid-item'>
+            <a href="">
+              <div className="poker-image-stack">
+                <img src="/ProductImages/poker/poker-bg.png" alt="Poker bg" className="poker-stack-img poker-stack-bg" />
+                <img src="/ProductImages/poker/poker-glow.png" alt="Poker Glow" className="poker-stack-img poker-stack-glow" />
+                <img src="/ProductImages/poker/poker-card1.png" alt="Poker Cards" className="poker-stack-img poker-stack-card1" />
+                <img src="/ProductImages/poker/poker-card2.png" alt="Poker Cards" className="poker-stack-img poker-stack-card2" />
+                <img src="/ProductImages/poker/poker-card3.png" alt="Poker Cards" className="poker-stack-img poker-stack-card3" />
+                <img src="/ProductImages/poker/poker-card4.png" alt="Poker Cards" className="poker-stack-img poker-stack-card4" />
+                <img src="/ProductImages/poker/poker-chip-left-top.png" alt="Poker Chips Left" className="poker-stack-img poker-chip-left-top" />
+                <img src="/ProductImages/poker/poker-chip-left-bottom.png" alt="Poker Chips Left" className="poker-stack-img poker-chip-left-bottom" />
+                <img src="/ProductImages/poker/poker-chip-right-top.png" alt="Poker Chips Left" className="poker-stack-img poker-chip-right-top" />
+                <img src="/ProductImages/poker/poker-chip-right-bottom.png" alt="Poker Chips Left" className="poker-stack-img poker-chip-right-bottom" />
+                <img src="/ProductImages/poker/poker-text.png" alt="Poker Text" className="poker-stack-img poker-stack-text" />
+              </div>
+            </a>
+      </div>
+      <div className='product-grid-item'>
+            <a href="">
+              <div className="poker-image-stack">
+                <img src="/ProductImages/poker/poker-bg.png" alt="Poker bg" className="poker-stack-img poker-stack-bg" />
+                <img src="/ProductImages/poker/poker-glow.png" alt="Poker Glow" className="poker-stack-img poker-stack-glow" />
+                <img src="/ProductImages/poker/poker-card1.png" alt="Poker Cards" className="poker-stack-img poker-stack-card1" />
+                <img src="/ProductImages/poker/poker-card2.png" alt="Poker Cards" className="poker-stack-img poker-stack-card2" />
+                <img src="/ProductImages/poker/poker-card3.png" alt="Poker Cards" className="poker-stack-img poker-stack-card3" />
+                <img src="/ProductImages/poker/poker-card4.png" alt="Poker Cards" className="poker-stack-img poker-stack-card4" />
+                <img src="/ProductImages/poker/poker-chip-left-top.png" alt="Poker Chips Left" className="poker-stack-img poker-chip-left-top" />
+                <img src="/ProductImages/poker/poker-chip-left-bottom.png" alt="Poker Chips Left" className="poker-stack-img poker-chip-left-bottom" />
+                <img src="/ProductImages/poker/poker-chip-right-top.png" alt="Poker Chips Left" className="poker-stack-img poker-chip-right-top" />
+                <img src="/ProductImages/poker/poker-chip-right-bottom.png" alt="Poker Chips Left" className="poker-stack-img poker-chip-right-bottom" />
+                <img src="/ProductImages/poker/poker-text.png" alt="Poker Text" className="poker-stack-img poker-stack-text" />
+              </div>
+            </a>
+      </div>
+      <div className='product-grid-item'>
+            <a href="">
+              <div className="poker-image-stack">
+                <img src="/ProductImages/poker/poker-bg.png" alt="Poker bg" className="poker-stack-img poker-stack-bg" />
+                <img src="/ProductImages/poker/poker-glow.png" alt="Poker Glow" className="poker-stack-img poker-stack-glow" />
+                <img src="/ProductImages/poker/poker-card1.png" alt="Poker Cards" className="poker-stack-img poker-stack-card1" />
+                <img src="/ProductImages/poker/poker-card2.png" alt="Poker Cards" className="poker-stack-img poker-stack-card2" />
+                <img src="/ProductImages/poker/poker-card3.png" alt="Poker Cards" className="poker-stack-img poker-stack-card3" />
+                <img src="/ProductImages/poker/poker-card4.png" alt="Poker Cards" className="poker-stack-img poker-stack-card4" />
+                <img src="/ProductImages/poker/poker-chip-left-top.png" alt="Poker Chips Left" className="poker-stack-img poker-chip-left-top" />
+                <img src="/ProductImages/poker/poker-chip-left-bottom.png" alt="Poker Chips Left" className="poker-stack-img poker-chip-left-bottom" />
+                <img src="/ProductImages/poker/poker-chip-right-top.png" alt="Poker Chips Left" className="poker-stack-img poker-chip-right-top" />
+                <img src="/ProductImages/poker/poker-chip-right-bottom.png" alt="Poker Chips Left" className="poker-stack-img poker-chip-right-bottom" />
+                <img src="/ProductImages/poker/poker-text.png" alt="Poker Text" className="poker-stack-img poker-stack-text" />
+              </div>
+            </a>
       </div>
       
-      <div className='product-grid-item'>
-
-            <a href="">
-              <div className="poker-image-stack">
-                <img src="/ProductImages/poker/poker-bg.png" alt="Poker BG" className="poker-stack-img poker-stack-bg" />
-                <img src="/ProductImages/poker/poker-glow.png" alt="Poker Glow" className="poker-stack-img poker-stack-glow" />
-                <img src="/ProductImages/poker/poker-cards.png" alt="Poker Cards" className="poker-stack-img poker-stack-cards" />
-                <img src="/ProductImages/poker/poker-chips-left.png" alt="Poker Chips Left" className="poker-stack-img poker-stack-chips-left" />
-                <img src="/ProductImages/poker/poker-chips-right.png" alt="Poker Chips Right" className="poker-stack-img poker-stack-chips-right" />
-                <img src="/ProductImages/poker/poker-text.png" alt="Poker Text" className="poker-stack-img poker-stack-text" />
-              </div>
-            </a>
-
-      </div>
-
-      <div className='product-grid-item'>
-
-            <a href="">
-              <div className="poker-image-stack">
-                <img src="/ProductImages/poker/poker-bg.png" alt="Poker BG" className="poker-stack-img poker-stack-bg" />
-                <img src="/ProductImages/poker/poker-glow.png" alt="Poker Glow" className="poker-stack-img poker-stack-glow" />
-                <img src="/ProductImages/poker/poker-cards.png" alt="Poker Cards" className="poker-stack-img poker-stack-cards" />
-                <img src="/ProductImages/poker/poker-chips-left.png" alt="Poker Chips Left" className="poker-stack-img poker-stack-chips-left" />
-                <img src="/ProductImages/poker/poker-chips-right.png" alt="Poker Chips Right" className="poker-stack-img poker-stack-chips-right" />
-                <img src="/ProductImages/poker/poker-text.png" alt="Poker Text" className="poker-stack-img poker-stack-text" />
-              </div>
-            </a>
-
-      </div>
-
-      <div className='product-grid-item'>
-
-            <a href="">
-              <div className="poker-image-stack">
-                <img src="/ProductImages/poker/poker-bg.png" alt="Poker BG" className="poker-stack-img poker-stack-bg" />
-                <img src="/ProductImages/poker/poker-glow.png" alt="Poker Glow" className="poker-stack-img poker-stack-glow" />
-                <img src="/ProductImages/poker/poker-cards.png" alt="Poker Cards" className="poker-stack-img poker-stack-cards" />
-                <img src="/ProductImages/poker/poker-chips-left.png" alt="Poker Chips Left" className="poker-stack-img poker-stack-chips-left" />
-                <img src="/ProductImages/poker/poker-chips-right.png" alt="Poker Chips Right" className="poker-stack-img poker-stack-chips-right" />
-                <img src="/ProductImages/poker/poker-text.png" alt="Poker Text" className="poker-stack-img poker-stack-text" />
-              </div>
-            </a>
-
-      </div>
+     
     </div>
   )
 }
