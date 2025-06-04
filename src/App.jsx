75884d05e602535b99489a266c5f781b2cbccba3
Aman <amank.business@outlook.com>
2025-06-04 18:50:33 +0530

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,31 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Suspense, lazy } from 'react'
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
-import Home from './pages/Home/Home'
-import SkillGames from './pages/SkillGames/SkillGames'
-import CasinoGames from './pages/CasinoGames/CasinoGames'
-import GameAPIs from './pages/GameAPIs/GameAPIs'
-import WhiteLabel from './pages/WhiteLabel/WhiteLabel'
+import LoadingSpinner from './components/LoadingSpinner/LoadingSpinner'
+
+// Lazy load route components
+const Home = lazy(() => import('./pages/Home/Home'))
+const SkillGames = lazy(() => import('./pages/SkillGames/SkillGames'))
+const CasinoGames = lazy(() => import('./pages/CasinoGames/CasinoGames'))
+const GameAPIs = lazy(() => import('./pages/GameAPIs/GameAPIs'))
+const WhiteLabel = lazy(() => import('./pages/WhiteLabel/WhiteLabel'))
 
 function App() {
   return (
     <Router>
       <div className='App'>
         <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/skill-games/*" element={<SkillGames />} />
-          <Route path="/casino-games/*" element={<CasinoGames />} />
-          <Route path="/game-apis/*" element={<GameAPIs />} />
-          <Route path="/white-label/*" element={<WhiteLabel />} />
-        </Routes>
+        <Suspense fallback={<LoadingSpinner />}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/skill-games/*" element={<SkillGames />} />
+            <Route path="/casino-games/*" element={<CasinoGames />} />
+            <Route path="/game-apis/*" element={<GameAPIs />} />
+            <Route path="/white-label/*" element={<WhiteLabel />} />
+          </Routes>
+        </Suspense>
         <Footer />
       </div>
     </Router>
